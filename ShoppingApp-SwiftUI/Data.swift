78e594091be985ad781b90f576e528ba5e36c757
Onur Fidan <97627723+Onurfdn@users.noreply.github.com>
2023-08-26 20:34:28 +0300

    new frame

---
+++ ShoppingApp-SwiftUI/Data.swift
@@ -0,0 +1,18 @@
+//
+//  Data.swift
+//  ShoppingApp-SwiftUI
+//
+//  Created by Onur Fidan on 26.08.2023.
+//
+
+import SwiftUI
+
+var shopItem: [[Any]] = [
+    ["avocado", "Avocado", 4.00, Color.green],
+    ["banana", "Banana", 2.50, Color.yellow],
+    ["chicken", "Chicken", 12.80, Color.red],
+    ["water", "Water", 1.00, Color.blue]
+]
+
+var cartItems : [[Any]] = [
+]
---
+++ ShoppingApp-SwiftUI/ShopItem.swift
@@ -0,0 +1,47 @@
+//
+//  ShopItem.swift
+//  ShoppingApp-SwiftUI
+//
+//  Created by Onur Fidan on 26.08.2023.
+//
+
+import SwiftUI
+
+struct ShopItem: View {
+    var imageName : String
+    var title: String
+    var price: Double
+    var color: Color
+    var selfIndex: Int
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 15)
+                .foregroundColor(color)
+                .opacity(0.25)
+                .frame(width: 170, height: 230)
+            
+            VStack{
+                
+                Image(imageName)
+                    .resizable()
+                    .frame(width: 80, height: 80)
+                Text(title)
+                Button() {
+                    cartItems.append(shopItem[selfIndex])
+                } label: {
+                    Text("$\(String(format: "%.2f", price))")
+                        .foregroundColor(.white)
+                        .frame(width: 100, height: 40)
+                        .background(color)
+                }
+            }
+        }
+    }
+}
+
+struct ShopItem_Previews: PreviewProvider {
+    static var previews: some View {
+        ShopItem(imageName: "avocado", title: "Avocado", price: 4.00, color: Color.green, selfIndex: 0)
+    }
+}
