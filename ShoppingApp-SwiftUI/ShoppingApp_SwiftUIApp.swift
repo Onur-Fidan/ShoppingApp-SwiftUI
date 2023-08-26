1b82712ebd099328b73379dd2f1120fbf7db7cce
Onur Fidan <97627723+Onurfdn@users.noreply.github.com>
2023-08-26 16:55:36 +0300

    Initial Commit

---
+++ ShoppingApp-SwiftUI/ShoppingApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingApp_SwiftUIApp.swift
+//  ShoppingApp-SwiftUI
+//
+//  Created by Onur Fidan on 26.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
